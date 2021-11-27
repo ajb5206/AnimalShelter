1e7035a580766609690d8d322e984078af14ee89
Alex Bertotto <ajb5206@gmail.com>
2021-11-27 13:58:15 -0500

    add cat model file with code

---
+++ AnimalShelter/Models/Cat.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelter.Models
+{
+	public class Cat
+	{
+		public int CatId { get; set; }
+		public string CatName { get; set; }
+		public int CatAge { get; set; }
+		public string CatGender { get; set; }
+		public string CatBreed { get; set; }
+	}
+
+}
