712411801ae2e3921c7d53224ef7234226ae78bd
Alex Bertotto <ajb5206@gmail.com>
2021-11-27 17:20:28 -0500

    add update and delete functionality

--- AnimalShelter/Controllers/DogsController.cs
+++ AnimalShelter/Controllers/DogsController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
@@ -45,5 +46,53 @@ namespace AnimalShelter.Controllers
 			return CreatedAtAction(nameof(GetDog), new { id = dog.DogId }, dog);
 		}
 
+		[HttpPut("{id}")]
+		public async Task<IActionResult> Put(int id, Dog dog)
+		{
+			if (id != dog.DogId)
+			{
+				return BadRequest();
+			}
+
+			_db.Entry(dog).State = EntityState.Modified;
+
+			try
+			{
+				await _db.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!DogExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteDog(int id)
+		{
+			var dog = await _db.Dogs.FindAsync(id);
+			if (dog == null)
+			{
+				return NotFound();
+			}
+
+			_db.Dogs.Remove(dog);
+			await _db.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		private bool DogExists(int id)
+		{
+			return _db.Dogs.Any(d => d.DogId == id);
+		}
 	}
 }
