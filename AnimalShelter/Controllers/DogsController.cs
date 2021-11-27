d5df6fd1c3dd67baf1c1535c1070f50d3e9fa457
Alex Bertotto <ajb5206@gmail.com>
2021-11-27 17:00:52 -0500

    add get route for single dog by Id and update post route with getdog

--- AnimalShelter/Controllers/DogsController.cs
+++ AnimalShelter/Controllers/DogsController.cs
@@ -23,13 +23,27 @@ namespace AnimalShelter.Controllers
 			return await _db.Dogs.ToListAsync();
 		}
 
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Dog>> GetDog(int id)
+		{
+			var dog = await _db.Dogs.FindAsync(id);
+
+			if (dog == null)
+			{
+				return NotFound();
+			}
+
+			return dog;
+		}
+
 		[HttpPost]
 		public async Task<ActionResult<Dog>> Post(Dog dog)
 		{
 			_db.Dogs.Add(dog);
 			await _db.SaveChangesAsync();
 
-			return CreatedAtAction("Post", new { id = dog.DogId }, dog);
+			return CreatedAtAction(nameof(GetDog), new { id = dog.DogId }, dog);
 		}
+
 	}
 }
