068fa2d8ab38b570944e0bc6d08d36820783340f
Alex Bertotto <ajb5206@gmail.com>
2021-11-27 13:57:55 -0500

    add dog model file with code

---
+++ AnimalShelter/Models/Dog.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelter.Models
+{
+	public class Dog
+	{
+		public int DogId { get; set; }
+		public string DogName { get; set; }
+		public int DogAge { get; set; }
+		public string DogGender { get; set; }
+		public string DogBreed { get; set; }
+	}
+
+}
