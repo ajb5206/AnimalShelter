43f15a89e25ed385b53905fd7b5beb88a77eb397
Alex Bertotto <ajb5206@gmail.com>
2021-11-27 22:59:38 -0500

    remove extraneous stuff from pagniationModel

--- AnimalShelter/Models/PaginationModel.cs
+++ AnimalShelter/Models/PaginationModel.cs
@@ -5,20 +5,7 @@ namespace AnimalShelter.Controllers
 {
 	public class PaginationModel
 	{
-		//public List<Dog> DogData { get; set; }
-		// public int DogTotal { get; set;}
-		// public int DogPerPage { get; set;}
-		// public int DogPage { get; set; }
-		// public string DogPreviousPage { get; set; }
-		// public string DogNextPage { get; set; }
-
 		public List<Cat> CatData { get; set; }
-		// public int CatTotal { get; set; }
-		// public int CatPerPage { get; set; }
-		// public int CatPage { get; set; }
-		// public string CatPreviousPage { get; set; }
-		// public string CatNextPage { get; set; }
-
 		public int Total { get; set;}
 		public int PerPage { get; set;}
 		public int Page { get; set;}
